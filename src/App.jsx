15aa8492956b8c98de0a8391e97c4b376e0d0935
fardinariful <fardinewucse2020@gmail.com>
2024-11-04 09:37:48 +0600

    basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className='text-6xl bg-red-500 '>Knowledge cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
